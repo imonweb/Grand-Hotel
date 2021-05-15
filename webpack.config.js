8927d47687dd3f1b3d4b372b9b2474d90a1acba9
imonweb <contact@imonweb.co.uk>
2021-05-16 00:09:05 +0100

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'myBundle.js'
+  },
+  devServer: {
+    before: function(src,server){
+      server._watch('./src/*.html')
+    },
+    contentBase: path.join(__dirname,'src'),
+    hot: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader","css-loader"]
+      },
+       {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
